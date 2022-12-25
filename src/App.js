d84ee2fdf18088b69e1f1d98f9f54329e953f829
Dyary Yassin <dyaryyasin165@gmail.com>
2022-12-25 20:53:36 +0300

    React Router

--- src/App.js
+++ src/App.js
@@ -4,13 +4,21 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import SearchParams from "./SearchParams";
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Details from "./Details";
 
 const App = () => {
   return (
-    <div>
-      <h1>Adopt ME</h1>
-      <SearchParams />
-    </div>
+    <BrowserRouter>
+    
+    
+        <h1>Adopt ME</h1>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+     
+    </BrowserRouter>
   )
 };
 
---
+++ src/Details.jsx
@@ -0,0 +1,4 @@
+const Details = () => {
+    return <h2>h1!</h2>
+};
+export default Details;
